2ad28234479e195e8a35fc46922b1a8a8153a6ca
alexkarevoll <alex.karevoll@gmail.com>
2017-02-01 11:45:11 -0800

    simple api call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAfUKgFJzLzctnVSmyXgOXOCQ5wW-3ZrzY'
+
+YTSearch({key: API_KEY, term:'surboards'}, function(data){
+  console.log(data)
+})
 
 // Create a new component that should produce some HTML
 const App = () => {
