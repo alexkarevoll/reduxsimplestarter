f8293f839b5bf86bbeb5e653f074ff61f005ff54
alexkarevoll <alex.karevoll@gmail.com>
2017-02-01 11:55:42 -0800

    set videos prop for parent class

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyAfUKgFJzLzctnVSmyXgOXOCQ5wW-3ZrzY'
 
-YTSearch({key: API_KEY, term:'surboards'}, function(data){
-  console.log(data)
-})
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-// Create a new component that should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+    // initialize the state with a property of videos set to an empty array
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term:'surboards'}, (videos) => { //videos = data
+      // update the state's videos property with the data from the api call
+      this.setState({ videos }) // this.setState({ videos: videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 // Take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
